ca6f18aefe347c90352ce4012a20f0acb3f1e0f5
EneasMenendez <eneasdmp36@educastur.es>
2025-09-26 13:24:57 +0200

    Nuevos cambios

--- 1_TRIMESTRE/REACT/Ejercicio_01/src/menu.jsx
+++ 1_TRIMESTRE/REACT/Ejercicio_01/src/menu.jsx
@@ -4,21 +4,31 @@ function Menu() {
 
       <nav class="navbar navbar-expand-lg navbar-dark bg-success">
         <div class="container">
-          <a class="navbar-brand" href="#">Naturaleza Viva</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+
+          <a class="navbar-brand fw-bold" href="#">Naturaleza Viva</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown">
             <span class="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ms-auto">
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav ms-3">
               <li class="nav-item">
                 <a class="nav-link active" href="#">Inicio</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="#">Proyectos</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Recursos</a>
+
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="recursosDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Recursos
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="recursosDropdown">
+                  <li><a class="dropdown-item" href="#">Artículos</a></li>
+                  <li><a class="dropdown-item" href="#">Guías</a></li>
+                  <li><a class="dropdown-item" href="#">Videos</a></li>
+                </ul>
               </li>
+
               <li class="nav-item">
                 <a class="nav-link" href="#">Contacto</a>
               </li>
