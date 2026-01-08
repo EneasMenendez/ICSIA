ec88032751e9ba2511b83d2c75df8077248f5457
EneasMenendez <eneasdmp36@educastur.es>
2026-01-08 16:00:00 +0100

    Ejer_04 - Estado y props con Bootstrap

---
+++ Ejer_04/Ej02/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Dashboard from "./components/Dashboard.jsx";
+
+function App() {
+  return (
+    <>
+      <Dashboard />
+    </>
+  );
+}
+
+export default App;
+
